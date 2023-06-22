ebe389715d31775c3b3f999ed3925f633f66356a
alenaranjo <alenaranjo777@hotmail.com>
2023-06-21 18:11:19 -0600

    segunda entrega

--- code/script.js
+++ code/script.js
@@ -33,7 +33,6 @@ solicitarTipoAuto();
 
 function solicitarTipoAuto() {
     const tipoAuto = prompt("Ingrese el tipo de auto que desea comprar (Automovil, Camioneta Pick Up, Microbus):").toLowerCase();
-
     const presupuesto = parseInt(prompt("Ingrese su presupuesto:"));
 
     const autos = [
@@ -42,23 +41,27 @@ function solicitarTipoAuto() {
         { tipo: "microbus", precio: 37000 }
     ];
 
+    let autoSeleccionado;
+
     switch (tipoAuto) {
         case "automovil":
-            comprarAuto(autos[0], presupuesto);
+            autoSeleccionado = autos[0];
             break;
 
         case "camioneta pick up":
-            comprarAuto(autos[1], presupuesto);
+            autoSeleccionado = autos[1];
             break;
 
         case "microbus":
-            comprarAuto(autos[2], presupuesto);
+            autoSeleccionado = autos[2];
             break;
 
         default:
             mostrarError();
-            break;
+            return;
     }
+
+    comprarAuto(autoSeleccionado, presupuesto);
 }
 
 function mostrarError() {
@@ -72,7 +75,7 @@ function comprarAuto(auto, presupuesto) {
         alert("Felicidades, " + nombre + "! Usted puede comprar un " + auto.tipo + ". ¡Acérquese a nuestra agencia para elegir su modelo!");
         return;
     }
-    
+
     const faltante = auto.precio - presupuesto;
 
     if (faltante > 0) {
@@ -89,8 +92,57 @@ function comprarAuto(auto, presupuesto) {
 
             alert("Perfecto, " + nombre + ". Usted puede acercarse a nuestra sucursal para completar el proceso de financiamiento.");
             alert("El plazo de financiamiento será de " + plazoFinanciamiento + " años y la tasa de interés será del " + tasaInteres + "% anual.");
+        } else if (aceptaFinanciamiento === "no") {
+            alert("Gracias por su tiempo, " + nombre + ". De igual manera dejanos tus datos para tenerte en nuestra base.");
         } else {
-            alert("Gracias por su tiempo, " + nombre + ". Esperamos poder hacer negocios en otra oportunidad.");
+            alert("Opción inválida. Gracias por su tiempo, " + nombre + ".");
         }
     }
 }
+
+
+//Second Phase
+const nombreMarca = prompt("Ingrese La Marca:\n1. Toyota\n2. Nissan\n3. Ford").toLowerCase();
+
+let opcionSeleccionada;
+
+switch (nombreMarca) {
+  case "1":
+  case "toyota":
+    opcionSeleccionada = "Toyota";
+    alert("Escogiste la Marca Toyota");
+    break;
+  case "2":
+  case "nissan":
+    opcionSeleccionada = "Nissan";
+    alert("Escogiste la Marca Nissan");
+    break;
+  case "3":
+  case "ford":
+    opcionSeleccionada = "Ford";
+    alert("Escogiste la Marca Ford");
+    break;
+  default:
+    opcionSeleccionada = "Opción inválida";
+    break;
+}
+
+const persona = {};
+
+const datosIngresados = prompt("Ingrese estos datos nuevamente para proceso de compra\n1. Nombre\n2. Apellido\n3. Edad\n4. Numero de Telefono").toLowerCase();
+
+const valores = datosIngresados.split(",");
+
+
+persona.nombre = valores[0];
+persona.apellido = valores[1];
+persona.edad = valores[2];
+persona.numeroTelefono = valores[3];
+
+var msge1 = "Gracias, te tendremos en nuestra base con estos datos:" +
+            "\nNombre: " + persona.nombre +
+            "\nApellido: " + persona.apellido +
+            "\nEdad: " + persona.edad +
+            "\nNumero de Telefono: " + persona.numeroTelefono;
+
+alert(msge1);
